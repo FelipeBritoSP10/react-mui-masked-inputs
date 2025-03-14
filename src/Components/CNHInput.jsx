b40abffc61733165cc32174e3eb72dd33f2e2d0c
FelipeBritoSP10 <felipe.fab8@gmail.com>
2025-03-14 11:34:38 -0300

    Implementação do campo CNH

---
+++ src/Components/CNHInput.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useRef } from "react";
+import { TextField, Box } from "@mui/material";
+import IMask from "imask";
+
+const CNHInput = ({ value, onChange }) => {
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    const inputElement = inputRef.current?.querySelector("input");
+    if (!inputElement) return;
+
+    const mask = IMask(inputElement, { 
+      mask: "00000000000",
+      maxLength: 11,
+    });
+
+    mask.on("accept", () => {
+      if (mask.value.length <= 11) {
+        onChange(mask.value);
+      }
+    });
+
+    return () => mask.destroy();
+  }, [onChange]);
+
+  return (
+    <Box display="flex" justifyContent="center" mt={3}>
+      <TextField 
+        label="CNH" 
+        variant="outlined" 
+        inputRef={inputRef} 
+        defaultValue={value} 
+        inputProps={{ maxLength: 11 }} // Adicionando restrição extra no input
+      />
+    </Box>
+  );
+};
+
+export default CNHInput;
