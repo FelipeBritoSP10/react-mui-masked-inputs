e2a91f47169c5deb22ad1bcb06d09d27adc93c7b
FelipeBritoSP10 <felipe.fab8@gmail.com>
2025-03-14 11:42:20 -0300

    Implementação da Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,48 @@
 import React, { useState } from "react";
 import { Box, Stack } from "@mui/material";
+import NavBar from "./Layout/NavBar";
 import CPFInput from "./Components/Cpfinput";
 import CnpjInput from "./Components/CNPJInput";
 import CEPInput from "./Components/CEPInput";
 import DateInput from "./Components/DateInput";
+import PhoneInput from "./Components/PhoneInput";
+import CNHInput from "./Components/CNHInput";
 
 function App() {
   const [cpf, setCpf] = useState("");
   const [cnpj, setCnpj] = useState("");
   const [cep, setCep] = useState("");
   const [date, setDate] = useState("");
+  const [phone, setPhone] = useState("");
+  const [cnh, setCnh] = useState("");
 
   return (
-    <Box
-      sx={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100vh", // Ocupa toda a altura da tela
-        width: "100vw",  // Ocupa toda a largura da tela
-      }}
-    >
-      {/* Stack organiza os inputs um abaixo do outro */}
-      <Stack spacing={2} width="300px">
-        <CPFInput value={cpf} onChange={setCpf} />
-        <CnpjInput value={cnpj} onChange={setCnpj} />
-        <CEPInput value={cep} onChange={setCep}/>
-        <DateInput value={date} onChange={setDate}/>
-      </Stack>
-    </Box>
+    <>
+      {/* NavBar */}
+      <NavBar />
+
+      {/* Mantendo os inputs centralizados */}
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh", 
+          width: "100vw",  
+          flexDirection: "column", // Coloca a Stack abaixo da NavBar
+          marginTop: "10px", // Evita sobreposição com a NavBar
+        }}
+      >
+        <Stack spacing={2} width="300px">
+          <CPFInput value={cpf} onChange={setCpf} />
+          <CnpjInput value={cnpj} onChange={setCnpj} />
+          <CEPInput value={cep} onChange={setCep} />
+          <DateInput value={date} onChange={setDate} />
+          <PhoneInput value={phone} onChange={setPhone} />
+          <CNHInput value={cnh} onChange={setCnh} />
+        </Stack>
+      </Box>
+    </>
   );
 }
 
---
+++ src/Layout/NavBar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { AppBar, Toolbar, Typography, Container } from "@mui/material";
+
+const NavBar = () => {
+  return (
+    <AppBar position="static" color="primary">
+      <Toolbar>
+        <Container maxWidth="md" style={{ display: "flex", justifyContent: "center" }}>
+          <Typography variant="h6" component="div">
+            Inputs Formatados
+          </Typography>
+        </Container>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default NavBar;
